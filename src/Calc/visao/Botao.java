15634e618df2245af3f125caa13a64828228fce9
Lucas <lucasatletico@hotmail.com>
2020-05-08 18:34:31 -0300

    Criação da parte grafica da calculadora, layout

---
+++ src/Calc/visao/Botao.java
@@ -0,0 +1,19 @@
+package Calc.visao;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class Botao extends JButton{
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setFont(new Font("courrier", Font.PLAIN, 20));		
+		setOpaque(true);
+		setBackground(cor);
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
+}
